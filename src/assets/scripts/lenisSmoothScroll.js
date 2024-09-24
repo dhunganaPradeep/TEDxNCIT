0b1dafebeaf86905e066da89b9eeb1fdedb6fb8e
dhunganaPradeep <pradeep.dhungana23@gmail.com>
2024-09-24 12:26:33 +0545

    layout

--- src/assets/scripts/lenisSmoothScroll.js
+++ src/assets/scripts/lenisSmoothScroll.js
@@ -1,5 +1,5 @@
 import "@styles/lenis.css";
-
+// import gsap from "gsap";
 import Lenis from "lenis";
 
 // Script to handle Lenis library settings for smooth scrolling
@@ -13,3 +13,19 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 
+// // Select wave elements
+// const waves = document.querySelectorAll('.wave');
+
+// window.addEventListener('scroll', () => {
+//   const scrollY = window.scrollY;
+
+//   // Apply parallax effect based on scroll position
+//   waves.forEach((wave, index) => {
+//     const speed = 0.2 + index * 0.1; // Different speed for each wave
+//     gsap.to(wave, {
+//       y: scrollY * speed,
+//       duration: 0.5,
+//       ease: "power1.out"
+//     });
+//   });
+// });
